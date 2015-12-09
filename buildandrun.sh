61f08c981c196aa044e3f706b82dbc0f79b656c8
Barry <barry.teoh@perceptionz.net>
2015-12-09 14:28:48 +0900

    New directory structure

--- buildandrun.sh
+++ buildandrun.sh
@@ -7,4 +7,4 @@ DOCKERNAME=swiftdocker
 docker build -t $DOCKERUSER .
 # Expose the Sources directory and run an interactive shell inside the container
 # This can be changed to run an actual swift service
-docker run -v "$PWD/Sources":/src -it  --name $DOCKERNAME $DOCKERUSER
+docker run -v "$PWD/Project":/src -it  --name $DOCKERNAME $DOCKERUSER
