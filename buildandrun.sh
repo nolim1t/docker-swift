2984aa718c52d3bbe80b86d2a3e73be0241f033d
Barry <barry.teoh@perceptionz.net>
2015-12-09 14:38:13 +0900

    gitignore and directory structure

--- buildandrun.sh
+++ buildandrun.sh
@@ -3,8 +3,10 @@
 # Change this to something identifiable to you
 DOCKERUSER=nolim1t/swiftdocker
 DOCKERNAME=swiftdocker
+# This is the executable name when you run swift build
+PROJECTNAME=SwiftProject
 
 docker build -t $DOCKERUSER .
 # Expose the Sources directory and run an interactive shell inside the container
 # This can be changed to run an actual swift service
-docker run -v "$PWD/Project":/src -it  --name $DOCKERNAME $DOCKERUSER
+docker run -v "$PWD/Project":/$PROJECTNAME -it  --name $DOCKERNAME $DOCKERUSER
